84abe6681f545c9b6ec9461e8ffb622beadd08ad
Vladimir Kryvian <meffi@lab313.ru>
2016-03-18 21:20:20 +0300

    Reorganization step 3.

---
+++ include/ida_debug.h
@@ -0,0 +1,10 @@
+#ifndef IDA_DEBUG_H
+#define IDA_DEBUG_H
+
+HINSTANCE GetHInstance();
+inline unsigned int mask(unsigned char bit_idx, unsigned char bits_cnt = 1)
+{
+	return (((1 << bits_cnt) - 1) << bit_idx);
+}
+
+#endif
